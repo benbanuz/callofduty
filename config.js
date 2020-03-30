8f33561355c3dc312f141cbdd2c0f58518f85002
benbanuz <ben632@walla.com>
2020-03-30 15:50:13 +0300

    Implemented soldiers routs

--- config.js
+++ config.js
@@ -4,5 +4,9 @@ const url = 'mongodb://localhost:27017';
 // Database Name
 const dbName = 'myproject';
 
+//server url
+const serverUrl = "http://localhost:3000/";
+
 module.exports.dbUrl = url;
 module.exports.dbName = dbName;
+module.exports.serverUrl = serverUrl;
