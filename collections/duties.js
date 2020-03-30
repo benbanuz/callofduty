b88c141b4b67dd03bc78774882ac6d6109c0b40b
benbanuz <ben632@walla.com>
2020-03-30 16:41:56 +0300

    Implemented update dutie

--- collections/duties.js
+++ collections/duties.js
@@ -82,6 +82,46 @@ function deleteDutie(id, callback) {
   });
 }
 
+function updateDutie(data, id, callback) {
+  let flag = true;
+  let leagalKeys = ["name", "location", "days", "constraints", "soldiersRequired", "value", "soldiers"];
+  for (let key in data) {
+    if (!(leagalKeys.includes(key))) {
+      flag = false;
+    }
+  }
+
+  if (flag) {
+    MongoClient.connect(url, function (err, client) {
+      assert.equal(null, err);
+      const db = client.db(dbName);
+      const collection = db.collection('duties');
+
+      collection.find({
+        "_id": ObjectId(id)
+      }).toArray(function (err, doc) {
+        if (doc.length == 1 && doc[0]["soldiers"].length == 0) {
+          collection.updateOne({
+            "_id": ObjectId(id)
+          }, {
+            $set: data
+          }, function (err, result) {
+            assert.equal(err, null);
+            client.close();
+            callback(err, null);
+          });
+        } else {
+          client.close();
+          callback("dutie cannot be updated", null);
+        }
+      });
+    });
+  } else {
+    callback("ileagal properties to update", null);
+  }
+}
+
 module.exports.newDutie = newDutie;
 module.exports.getDutie = getDutie;
 module.exports.deleteDutie = deleteDutie;
+module.exports.updateDutie = updateDutie;
