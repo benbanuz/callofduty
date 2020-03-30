338e66d9e935d5b66749ebca34608c937a2cd5d0
benbanuz <ben632@walla.com>
2020-03-30 15:41:50 +0300

    Implemented server

---
+++ start.js
@@ -0,0 +1,61 @@
+const http = require("http");
+const soldierRoute = require("./routs/soldiers.js");
+const dutiesRoute = require("./routs/duties");
+const jbRoute = require("./routs/justiceBoard");
+
+function runServer() {
+  let server = http.createServer(function (req, res) {
+    let {
+      method,
+      url
+    } = req;
+
+    let body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      if (body != "") {
+        body = JSON.parse(body);
+      }
+      if (/soldiers.*/.test(url.split("/")[1]) && url.split("/").length <= 3) {
+        soldierRoute.soldierRequest(method, url, body, function (err, result) {
+          if (err) {
+            res.statusCode = 400;
+            res.end(err);
+          } else {
+            res.end(result);
+          }
+        });
+      } else if (/duties.*/.test(url.split("/")[1]) && url.split("/").length <= 4) {
+        if (method == "OPTIONS") {
+          method = req.headers["access-control-request-method"];
+        }
+        dutiesRoute.dutieRequest(method, url, body, function (err, result) {
+          if (err) {
+            res.statusCode = 400;
+            res.end(err);
+          } else {
+            res.end(result);
+          }
+        });
+      } else if (/justiceBoard/.test(url) && method == "GET") {
+        jbRoute.showBoard(function (err, board) {
+          if (err) {
+            res.statusCode = 400;
+            res.end(err);
+          } else {
+            res.end(board);
+          }
+        })
+      } else {
+        res.statusCode = 404;
+        res.end();
+      }
+    });
+  }).listen(3000);
+
+  return server;
+}
+
+module.exports.server = runServer();
